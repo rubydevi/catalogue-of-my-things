a917b6ec36c47f6a679552d39f22b03320a57e0e
iamrubydevi <ariesabyss@gmail.com>
2023-09-18 16:56:39 +0530

    Create a Book class in a separate file

---
+++ src/book.rb
@@ -0,0 +1,16 @@
+require_relative 'items'
+
+class Book < Items
+  attr_accessor :publisher, :cover_state, :publish_date
+  attr_reader :id
+
+  def initialize(publish_date, publisher, cover_state, archived: false)
+    super(publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super && @cover_state != 'damaged'
+  end
+end
