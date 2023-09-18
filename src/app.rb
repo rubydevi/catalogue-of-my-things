261e321d1e6e5cd5f246bcd06fbd407ee0f713ac
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-18 16:46:02 +0300

    update

--- src/app.rb
+++ src/app.rb
@@ -1,19 +1,57 @@
-class Menu
-  def display_options
-    puts 'Select an option :'
-    puts '1 - List all books'
-    puts '2 - List all music albums'
-    puts '3 - List of games'
-    puts '4 - List all genres'
-    puts '5 - List all labels'
-    puts '6 - List all authors'
-    puts '7 - Add a book'
-    puts '8 - Add a music album'
-    puts '9 - Add a game'
-    puts '10 = Exit app'
+class App
+  ACTIONS = {
+    1 => :find_books,
+    2 => :find_music_albums,
+    3 => :find_movies,
+    4 => :find_games,
+    5 => :find_genres,
+    6 => :find_labels,
+    7 => :find_authors,
+    8 => :find_sources,
+    9 => :add_a_book,
+    10 => :add_a_music_album,
+    11 => :add_a_movie,
+    12 => :add_a_game,
+    0 => :exit
+  }.freeze
+
+  def find_movies
+    puts 'Not implemented yet'
   end
 
-  def user_option
-    gets.chomp
+  def find_sources
+    puts 'Not implemented yet'
+  end
+
+  def add_a_movie
+    puts 'Not implemented yet'
+  end
+
+  def display_interactive_console
+    puts "\nPlease choose an option by entering a number:
+    1.  List all books
+    2.  List all music albums
+    3.  List all movies
+    4.  List all games
+    5.  List all genres
+    6.  List all labels
+    7.  List all authors
+    8.  List all sources
+    9.  Add a book
+    10. Add a music album
+    11. Add a movie
+    12. Add a game
+    0.  Exit"
+  end
+
+  def run
+    entry = -1
+    until entry.zero?
+      display_interactive_console
+      entry = gets.chomp.to_i
+      option = ACTIONS[entry]
+      option ? send(option) : puts('Invalid input')
+
+    end
   end
 end
--- src/main.rb
+++ src/main.rb
@@ -1,33 +1,8 @@
 require_relative 'app'
 
-menu = Menu.new
-
-puts 'Welcome to my catalog of things app'
-
-loop do
-  menu.display_options
-  case menu.user_option
-  when '1'
-    print '1 selected'
-  when '2'
-    print '2 selected'
-  when '3'
-    print '3 selected'
-  when '4'
-    print '4 selected'
-  when '5'
-    print '5 selected'
-  when '6'
-    print '6 selected'
-  when '7'
-    print '7 selected'
-  when '8'
-    print '8 selected'
-  when '9'
-    print '9 selected'
-  when '10'
-    print 'Thank you for using this app!'
-    break
-  end
-  puts "\n"
+def main
+  app = App.new
+  app.run
 end
+
+main
