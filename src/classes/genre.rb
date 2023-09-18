8824e84130e8632254f0299aa1ca36bddd437fc0
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-18 15:08:50 +0300

    fix linters

--- src/classes/genre.rb
+++ src/classes/genre.rb
@@ -1,15 +1,15 @@
 class Genre
-    attr_reader :id
-    attr_accessor :name, :items
-  
-    def initialize(name)
-      @id = Random.rand(1..100)
-      @name = name
-      @items = []
-    end
-  
-    def add_item(items)
-      @items << Items
-      items.genre = self
-    end
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..100)
+    @name = name
+    @items = []
   end
+
+  def add_item(items)
+    @items << Items
+    items.genre = self
+  end
+end
