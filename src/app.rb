88717637f0f7ca750c213006ff747ca79676c726
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-09-18 18:26:24 +0300

    Update app.rb

--- src/app.rb
+++ src/app.rb
@@ -1,4 +1,13 @@
+require './util/json_storage'
+require_relative 'functions/find_music_albums'
+require_relative 'functions/find_genres'
+require_relative 'functions/add_a_music_album'
+
 class App
+  def initialize
+    @music_albums = JSONStorage.load_data('music_albums').empty? ? [] : JSONStorage.load_data('music_albums')
+    @genres = JSONStorage.load_data('genres').empty? ? [] : JSONStorage.load_data('genres')
+  end
   ACTIONS = {
     1 => :find_books,
     2 => :find_music_albums,
@@ -51,6 +60,8 @@ class App
       entry = gets.chomp.to_i
       option = ACTIONS[entry]
       option ? send(option) : puts('Invalid input')
+      JSONStorage.save_data('music_albums', @music_albums)
+      JSONStorage.save_data('genres', @genres)
     end
   end
 end
