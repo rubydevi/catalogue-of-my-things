9d978425e5ce120a4288f4b9669e008c6ab12f6c
Yonashenok <yonashenok24@gmail.com>
2023-09-19 08:46:07 +0300

    feat: save authors and games in json format

--- src/store.rb
+++ src/store.rb
@@ -1,35 +1,48 @@
 class Store
-    def store(arr, data)
-        arr.each do |game|
+  def store_game(arr, data)
+    arr.each do |game|
+      data.push({
+                  id: game.id,
+                  name: game.name,
+                  multiplayer: game.multiplayer,
+                  last_played_at: game.last_played_at,
+                  author: {
+                    id: game.author.id,
+                    first_name: game.author.first_name,
+                    last_name: game.author.last_name
+                  },
+                  published_date: game.publish_date
+                })
+    end
+    data
+  end
 
-          data.push({
-            id: game.id,
-            name: game.name,
-            multiplayer: game.multiplayer,
-            last_played_at: game.last_played_at,
-            author: {
-              id: game.author.id,
-              first_name: game.author.first_name,
-              last_name: game.author.last_name
-            },
-            published_date: game.publish_date
-          })
-        end
-          data
-      end
-    
-      def store_game(arr)
+  def store_author(arr, data)
+    arr.each do |author|
+      data.push({
+                  id: author.id,
+                  first_name: author.first_name,
+                  last_name: author.last_name
+                })
+    end
+    data
+  end
 
-        if File.exist?('./data/games.json')
-          file = File.read('./data/games.json')
-          data = JSON.parse(file)
-        else
-          data = []
-        end
-    
-        if !File.exist?('./data/games.json')
-        File.new('./data/games.json', 'w+') 
-        end
-      File.write('./data/games.json', JSON.dump(store(arr, data)))
-      end
+  def store(arr, path)
+    if File.exist?(path)
+      file = File.read(path)
+      data = JSON.parse(file)
+    else
+      data = []
+    end
+
+    File.new(path, 'w+') unless File.exist?(path)
+
+    case path
+    when './data/games.json'
+      File.write(path, JSON.dump(store_game(arr, data)))
+    when './data/authors.json'
+      File.write(path, JSON.dump(store_author(arr, data)))
+    end
+  end
 end
