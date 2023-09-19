c5f0542bb6321d29027d924d4018998d7598c9b4
Yonashenok <yonashenok24@gmail.com>
2023-09-19 08:45:15 +0300

    feat: read saved data

--- src/filechecker.rb
+++ src/filechecker.rb
@@ -3,7 +3,7 @@ require 'json'
 # checks if file exists
 class FileChecker
   def self.read_json_file(file_path)
-    return [] unless File.exist?(file_path)
+    return '[]' unless File.exist?(file_path)
 
     File.read(file_path)
   end
