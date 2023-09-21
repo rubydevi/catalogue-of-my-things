f11bf7db33764154c9506029097ef7a601ffbb83
iamrubydevi <ariesabyss@gmail.com>
2023-09-21 21:30:44 +0530

    Make the changes for item and genre classes

--- src/classes/items.rb
+++ src/classes/items.rb
@@ -1,8 +1,8 @@
 require 'date'
 
 class Items
-  attr_accessor :publish_date, :archived, :label
-  attr_reader :genre, :author, :id
+  attr_accessor :publish_date, :archived
+  attr_reader :genre, :author, :label, :id
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1...1000)
@@ -10,30 +10,33 @@ class Items
     @archived = archived
   end
 
-  def add_genre(genre)
+  def genre=(genre)
     @genre = genre
-    genre.add_item(self) unless genre.items.include?(self)
+    genre.items.push(self) unless genre.items.include?(self)
   end
 
-  def add_author(author)
+  def author=(author)
     @author = author
+    author.items.push(self) unless author.items.include?(self)
   end
 
-  def add_source(_source)
-    @source = Source
-  end
-
-  def add_label(label)
+  def label=(label)
     @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items.push(self) unless source.items.include?(self)
   end
 
   def can_be_archived?
     current_date = Date.today
-    year = current_date - @publish_date.year
-    year >= 10
+    year = current_date.year - @publish_date.year
+    year > 10
   end
 
   def move_to_archive
-    @archived = can_be_archived?
+    @archived = true if can_be_archived?
   end
 end
